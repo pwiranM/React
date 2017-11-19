0d823319b405c572a2ad9d53d0c5a85e40509ad0
pwiranM <wiran.praew95@hotmail.com>
2017-11-19 15:59:03 +0700

    first commit

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import localStorage from 'localStorage'
+
+import Login from './pages/Login'
+import Main from './pages/Main'
+
+const Routes = () => (
+  <Switch>
+    <Route exact path="/login" component={Login} />
+    {!localStorage.getItem('username') ? (
+      <Redirect to="/login" />
+    ) : (
+      <Switch>
+        <Route exact path="/" component={Main} />
+      </Switch>
+    )}
+  </Switch>
+)
+
+export default Routes
